641fefb3fbc49370daa49d927d0658a4dbe5a0ab
jgonzalez <jmgonzalez@rnpn.gob.sv>
2016-09-21 13:34:28 -0600

    WebServices con RESTFul

---
+++ src/main/java/com/sv/udb/controlador/MiWebServices.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sv.udb.controlador;
+
+/**
+ *
+ * @author REGISTRO
+ */
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.sv.udb.utils.AlumnosCtrl;
+import com.sv.udb.utils.AlumnosResponse;
+import java.io.StringWriter;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+@Path("/MiServicio")
+public class MiWebServices {
+    @GET
+    @Path("/{user}/{pass}")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public Response getMsg(@PathParam("user") String user, @PathParam("pass") String pass) {
+        try
+        {
+            AlumnosResponse obje = new AlumnosCtrl().cons(user, pass);
+            ObjectMapper mapa = new ObjectMapper();
+            mapa.configure(SerializationFeature.INDENT_OUTPUT, true);
+            StringWriter sali = new StringWriter();
+            mapa.writeValue(sali, obje);
+            return Response.status(200).entity(sali.toString()).build();
+        }
+        catch(Exception ex)
+        {
+            return Response.status(200).entity("Error").build();
+        }
+    }
+}
---
+++ src/main/java/com/sv/udb/utils/AlumnosCtrl.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sv.udb.utils;
+
+import com.sv.udb.modelo.Usuarios;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author REGISTRO
+ */
+public class AlumnosCtrl {
+    public AlumnosResponse cons(String usua, String pass)
+    {
+        AlumnosResponse resp = new AlumnosResponse();
+        resp.setResp(false);
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("POOPU");
+        EntityManager em = emf.createEntityManager();
+        try
+        {
+            Query q = em.createQuery("SELECT u FROM Usuarios u WHERE u.acceUsua = :acceUsua AND u.contUsua = :contUsua AND u.estaUsua = :estaUsua", Usuarios.class);
+            q.setParameter("acceUsua", usua);
+            q.setParameter("contUsua", pass);
+            q.setParameter("estaUsua", 1);
+            List results = q.getResultList();
+            if (!results.isEmpty())
+            {
+                Usuarios obje = (Usuarios)results.get(0);
+                resp.setResp(true);
+                resp.setNomb(String.format("%s %s", obje.getCodiAlum().getNombAlum(), obje.getCodiAlum().getApelAlum()));
+                resp.setMail(obje.getCodiAlum().getMailAlum());
+                resp.setFechNaci(obje.getCodiAlum().getFechNaciAlum());
+                resp.setUsua(obje.getAcceUsua());
+            }
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        finally
+        {
+            em.close();
+            emf.close();            
+        }
+        return resp;
+    }
+}
